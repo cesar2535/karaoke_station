ad125d3ca81b267b512c9fe494b4d849afa0c966
Cesar Chen <cesar6134@gmail.com>
2015-09-03 22:41:51 +0800

    Added the List component

---
+++ app/components/List.js
@@ -0,0 +1,30 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class List extends Component {
+  static propTypes = {
+    loadingLabel: PropType.string.isRequired,
+    pageCount: PropTypes.number,
+    renderItem: PropTypes.func.isRequired,
+    items: PropTypes.array.isRequired,
+    isFetching: PropTypes.bool.isRequired
+  }
+
+  render() {
+    const {
+      isFetching, pageCount, items, renderItem, loadingLabel
+    } = this.props;
+
+    const isEmpty = items.length === 0;
+    if (isEmpty && isFetching) {
+      return (
+        <h2><i>{loadingLabel}</i></h2>
+      );
+    }
+
+    return (
+      <div>
+        {item.map(renderItem)}
+      </div>
+    );
+  }
+}
