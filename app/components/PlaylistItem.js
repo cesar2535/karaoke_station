02414de30205e22934f113e3148a9c057a11759c
Cesar Chen <cesar6134@gmail.com>
2015-09-07 18:38:51 +0800

    Modified the styles of `Playlist`

--- app/components/PlaylistItem.js
+++ app/components/PlaylistItem.js
@@ -2,11 +2,16 @@ import React, { Component, PropTypes } from 'react';
 
 export default class PlaylistItem extends Component {
   static propTypes = {
-    className: PropTypes.string
+    className: PropTypes.string,
+    song: PropTypes.shape({
+      title: PropTypes.string,
+      artist: PropTypes.string
+    })
   }
 
-  static defaultProp = {
-
+  static defaultProps = {
+    className: '',
+    song: {}
   }
 
   constructor(props) {
@@ -14,9 +19,11 @@ export default class PlaylistItem extends Component {
   }
 
   render() {
-    const { className } = this.props;
+    const { className, song } = this.props;
     return (
-      <div className={className}>
+      <div className={`Playlist-item ${className}`}>
+        <span className="Playlist-item-title">{song.title}</span>
+        <span className="Playlist-item-artist">{song.artist}</span>
       </div>
     );
   }
