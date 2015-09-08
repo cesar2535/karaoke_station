d54ba05d55ae90b825fe3cbee7a94c3802ab5a73
RothGo <roth1002@gmail.com>
2015-09-08 16:33:17 +0800

    book components

---
+++ app/components/SongBook.js
@@ -0,0 +1,27 @@
+import React, { Component, PropTypes } from 'react';
+
+let songBookFakeData = [
+	{
+		name: '歌星點歌',
+		id: '0'
+	},
+	{
+		name: '依語言點歌',
+		id: '1'
+	},
+	{
+		name: '依種類點歌',
+		id: '2'
+	},
+]
+
+export default class SongBook extends Component {
+	render() {
+    	// const { className, songs } = this.props;
+	    return (
+			<section>Hello World
+			</section>
+    );
+  }
+}
+
--- app/containers/App.js
+++ app/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import SongBook from '../components/SongBook';
 
 class App extends Component {
   constructor(props) {
@@ -12,6 +13,7 @@ class App extends Component {
     return (
       <div>
         <Header />
+        <SongBook />
         <Footer />
       </div>
     );
