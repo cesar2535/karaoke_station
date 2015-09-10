6fab5531435b35dbd8e667e7c8d5c45d9420320f
RothGo <roth1002@gmail.com>
2015-09-10 18:21:23 +0800

    list view nav layout

--- app/components/ListNav.js
+++ app/components/ListNav.js
@@ -4,7 +4,14 @@ export default class ListNav extends Component {
 	render() {
 		const { className } = this.props;
 		return (
-			<section className={className}>我是listnav</section>
+			<section className={className}>
+				<section>
+					<a href="#">
+						<ic className="ic ic_toolbar_sort_default_disable"></ic>
+						<ic className="ic ic_topbar_triangle_normal TopBar"></ic>
+					</a>
+				</section>
+			</section>
 		);
 	}
 }
