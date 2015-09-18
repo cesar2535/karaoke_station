58466f7f3fe8aebd1f6e8b9599357d844dba3707
roth <roth429@msn.com>
2015-09-18 16:24:14 +0800

    Update PlayerActionCreators.spec.js
    fixed ide Indent

--- app/test/actions/PlayerActionCreators.spec.js
+++ app/test/actions/PlayerActionCreators.spec.js
@@ -13,20 +13,20 @@ describe('PlayerActionCreators actions', () => {
   it('togglePlayPauseButtom should create TOGGLE_PLAY_PAUSE_BUTTOM action', () => {
     expect(actions.togglePlayPauseButtom('pause'))
     .toEqual({
-			type: types.TOGGLE_PLAY_PAUSE_BUTTOM,
+	type: types.TOGGLE_PLAY_PAUSE_BUTTOM,
       status: 'pause'
     });
 
     expect(actions.togglePlayPauseButtom('play'))
     .toEqual({
-			type: types.TOGGLE_PLAY_PAUSE_BUTTOM,
+	type: types.TOGGLE_PLAY_PAUSE_BUTTOM,
       status: 'play'
     });
   });
 
   it('play(\'play\') should create [ types.PLAY_REQUEST, types.PLAY_SUCCESS, types.PLAY_FAILURE ] action with endpoint /player/play', () => {
     expect(
-			actions.play('play')(action => action, fakeStore({}))[CALL_API]
+	actions.play('play')(action => action, fakeStore({}))[CALL_API]
     ).toEqual({
       types: [ types.PLAY_REQUEST, types.PLAY_SUCCESS, types.PLAY_FAILURE ],
       endpoint: '/player/play',
@@ -37,7 +37,7 @@ describe('PlayerActionCreators actions', () => {
 
   it('play(\'pause\') should create [ types.PLAY_REQUEST, types.PLAY_SUCCESS, types.PLAY_FAILURE ] action with endpoint /player/pause', () => {
     expect(
-			actions.play('pause')(action => action, fakeStore({}))[CALL_API]
+	actions.play('pause')(action => action, fakeStore({}))[CALL_API]
     ).toEqual({
       types: [ types.PLAY_REQUEST, types.PLAY_SUCCESS, types.PLAY_FAILURE ],
       endpoint: '/player/pause',
@@ -48,7 +48,7 @@ describe('PlayerActionCreators actions', () => {
 
   it('play(\'resume\') should create [ types.PLAY_REQUEST, types.PLAY_SUCCESS, types.PLAY_FAILURE ] action with endpoint /player/resume', () => {
     expect(
-			actions.play('resume')(action => action, fakeStore({}))[CALL_API]
+	actions.play('resume')(action => action, fakeStore({}))[CALL_API]
     ).toEqual({
       types: [ types.PLAY_REQUEST, types.PLAY_SUCCESS, types.PLAY_FAILURE ],
       endpoint: '/player/resume',
@@ -59,7 +59,7 @@ describe('PlayerActionCreators actions', () => {
 
   it('pause should create [ types.PAUSE_REQUEST, types.PAUSE_SUCCESS, types.PAUSE_FAILURE ] action', () => {
     expect(
-			actions.pause()(action => action, fakeStore({}))[CALL_API]
+	actions.pause()(action => action, fakeStore({}))[CALL_API]
     ).toEqual({
       types: [ types.PAUSE_REQUEST, types.PAUSE_SUCCESS, types.PAUSE_FAILURE ],
       endpoint: '/player/pause',
@@ -70,7 +70,7 @@ describe('PlayerActionCreators actions', () => {
 
   it('stop should create [ types.STOP_REQUEST, types.STOP_SUCCESS, types.STOP_FAILURE ] action', () => {
     expect(
-			actions.stop()(action => action, fakeStore({}))[CALL_API]
+	actions.stop()(action => action, fakeStore({}))[CALL_API]
     ).toEqual({
       types: [ types.STOP_REQUEST, types.STOP_SUCCESS, types.STOP_FAILURE ],
       endpoint: '/player/stop',
@@ -81,7 +81,7 @@ describe('PlayerActionCreators actions', () => {
 
   it('repeat should create [ types.REPLAY_REQUEST, types.REPLAY_SUCCESS, types.REPLAY_FAILURE ] action', () => {
     expect(
-			actions.repeat()(action => action, fakeStore({}))[CALL_API]
+    	actions.repeat()(action => action, fakeStore({}))[CALL_API]
     ).toEqual({
       types: [ types.REPLAY_REQUEST, types.REPLAY_SUCCESS, types.REPLAY_FAILURE ],
       endpoint: '/player/replay',
@@ -92,7 +92,7 @@ describe('PlayerActionCreators actions', () => {
 
   it('next should create [ types.NEXT_REQUEST, types.NEXT_SUCCESS, types.NEXT_FAILURE ] action', () => {
     expect(
-			actions.next()(action => action, fakeStore({}))[CALL_API]
+	actions.next()(action => action, fakeStore({}))[CALL_API]
     ).toEqual({
       types: [ types.NEXT_REQUEST, types.NEXT_SUCCESS, types.NEXT_FAILURE ],
       endpoint: '/player/next',
