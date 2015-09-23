010965f5977127e3ee2d50c3c2677094fce1f2b0
RothGo <roth1002@gmail.com>
2015-09-23 17:09:25 +0800

    modify webpack config for build folder to static/ and add it to git ignore file

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     './app/index'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'static'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
