f13f2e43b8a3e56893c3b038831d66de8f0e86e8
Cesar Chen <cesar6134@gmail.com>
2015-09-25 14:43:59 +0800

    Change the `location.host.split(':')[0]` to `location.hostname`

--- app/constants/Config.js
+++ app/constants/Config.js
@@ -1,8 +1,7 @@
-export const API_ROOT = 'http://' + location.host.split(':')[0] + ':5603/ktvstation/v1';
+export const API_ROOT = 'http://' + location.hostname + ':5603/ktvstation/v1';
 export const QTS_ROOT = '/apps/KTVStation/';
 export const ADD_BUTTOM = 'ADD_BUTTOM';
 export const INSERT_BUTTOM = 'INSERT_BUTTOM';
 export const ADD_FAVORITE_BUTTOM = 'ADD_FAVORITE_BUTTOM';
 export const REMOVE_BUTTOM = 'REMOVE_BUTTOM';
 export const REMOVE_FAVORITE_BUTTOM = 'REMOVE_FAVORITE_BUTTOM';
-
