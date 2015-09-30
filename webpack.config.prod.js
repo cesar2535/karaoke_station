28f54641423c82537978a03946f94d985a1ce66c
Cesar Chen <cesar6134@gmail.com>
2015-09-30 14:10:28 +0800

    Initialize `webpack`, `babel`, and `npm` configuration

---
+++ webpack.config.prod.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      exclude: /node_modules/,
+      include: path.join(__dirname, 'src')
+    }, {
+      test: /\.css$/,
+      loaders: ['style', 'css', 'postcss'],
+      exclude: /node_modules/
+    }]
+  }
+};
