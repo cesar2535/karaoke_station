70d3b42e36ee5e676330afd7d05ca6a2ba89d0ea
Cesar Chen <cesar6134@gmail.com>
2015-10-02 17:46:37 +0800

    Writing the api middleware

--- app/middleware/api.js
+++ app/middleware/api.js
@@ -10,7 +10,7 @@ export const Schemas = {
 
 };
 
-function callApi({ endpoint, schema, method, body }) {
+function callApi({ endpoint, schema, method, body, normalizable }) {
 
 }
 
@@ -19,4 +19,47 @@ export default store => next => action => {
   if (typeof callAPI === 'undefined') {
     return next(action);
   }
-}
+
+  let { endpoint } = callAPI;
+  const { schema, types, method, body, normalizable } = callAPI;
+
+  if (typeof endpoint === 'function') {
+    endpoint = endpoint(store.getStata());
+  }
+
+  if (typeof endpoint != 'string') {
+    throw new Error('Specify a string endpoint URL.');
+  }
+
+  if (!schema) {
+    throw new Error('Specify one of the exported Schemas.');
+  }
+
+  if (!Array.isArray(types) || types.length !== 3) {
+    throw new Error('Expected an array of three action types.');
+  }
+
+  if (!types.every( type => typeof type === 'string' )) {
+    throw new Error('Expected action types to be strings.');
+  }
+
+  function actionWith(data) {
+    const finalAction = Object.assign({}, action, data);
+    delete finalAction[CALL_API];
+    return finalAction;
+  }
+
+  const [ requestType, successType, failureType ] = types;
+  next( actionWith({ type: requestType }) );
+
+  return callApi({ endpoint, schema, method, body, normalizable }).then(
+    response => next(actionWith({
+      response,
+      type: successType
+    })),
+    error => next(actionWith({
+      type: failureType,
+      error: error.message || `Something bad happend.`
+    }))
+  );
+};
