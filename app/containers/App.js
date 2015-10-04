8ea901053446cdb555231c2b4e0a9c7203bbec7c
Cesar Chen <cesar6134@gmail.com>
2015-10-04 08:59:03 +0800

    Implemented the `Header` and `Footer`

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,12 +1,17 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
 class App extends Component {
   render() {
     const { children } = this.props;
     return (
       <div className={`App`}>
+        <Header />
         {children}
+        <Footer />
       </div>
     );
   }
