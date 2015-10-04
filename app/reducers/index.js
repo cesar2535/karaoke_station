c6df7e3218918ceccce6bb5097ef48bef902cc02
Cesar Chen <cesar6134@gmail.com>
2015-10-04 08:58:06 +0800

    Added `entities` and `errorMessage` reducers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,29 @@
 import { combineReducers } from 'redux';
+import merge from 'lodash/object/merge';
 import * as ActionTypes from '../constants/ActionTypes';
 
-const rootReducer = combineReducers({
+function entities(state = {}, action) {
+  if (action.response && action.response.entities) {
+    return merge({}, state, action.response.entities);
+  }
 
+  return state;
+}
+
+function errorMessage(state = null, action) {
+  const { type, error } = action;
+  if (type === ActionTypes.RESET_ERROR_MESSAGE) {
+    return null;
+  } else if (error) {
+    return action.error;
+  }
+
+  return state;
+}
+
+const rootReducer = combineReducers({
+  entities,
+  errorMessage
 });
 
 export default rootReducer;
