81e0a2fbda2b6cfa8e55e9176301e05e8df4639a
Cesar Chen <cesar6134@gmail.com>
2015-10-04 08:55:38 +0800

    Added `Header` & `Footer` components

---
+++ app/components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react'
+
+class Footer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <footer className={`Footer`}>
+        Footer
+      </footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ app/components/Header.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <header className={`Header`}>
+        Header
+      </header>
+    );
+  }
+}
+
+export default Header;
