4baf80851a0470aee05d5e4baf2604bc01a24556
Cesar Chen <cesar6134@gmail.com>
2015-10-05 13:51:13 +0800

    Added the `SONGS_FROM_FAVORITE` actions for favorite.

--- app/reducers/pagination.js
+++ app/reducers/pagination.js
@@ -5,7 +5,7 @@ import * as ActionTypes from '../constants/ActionTypes';
 const pagination = combineReducers({
   songsByKeyword: paginate({
     mapActionToKey: action => action.keywords,
-    types: []
+    types: [ActionTypes.SONGS_BY_KEYWORD_REQUEST, ActionTypes.SONGS_BY_KEYWORD_SUCCESS, ActionTypes.SONGS_BY_KEYWORD_FAILURE]
   }),
   songsByArtist: paginate({
     mapActionToKey: action => action.name,
@@ -16,8 +16,8 @@ const pagination = combineReducers({
     types: [ActionTypes.SONGS_BY_LANG_REQUEST, ActionTypes.SONGS_BY_LANG_SUCCESS, ActionTypes.SONGS_BY_LANG_FAILURE]
   }),
   songsFromFavorites: paginate({
-    mapActionToKey: action => action.name,
-    types: []
+    mapActionToKey: action => action.favorId,
+    types: [ActionTypes.SONGS_FROM_FAVORITE_REQUEST, ActionTypes.SONGS_FROM_FAVORITE_SUCCESS, ActionTypes.SONGS_FROM_FAVORITE_FAILURE]
   }),
   songsFromPlaylist: paginate({
     mapActionToKey: action => action.name,
