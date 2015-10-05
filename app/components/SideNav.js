6774c357de10a89883d15ee11eaa277efde1dc39
Cesar Chen <cesar6134@gmail.com>
2015-10-05 16:43:46 +0800

    Added the `HOST` configuration constant

--- app/components/SideNav.js
+++ app/components/SideNav.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 
-import { LOCALHOST } from '../constants/Config';
+import { HOST } from '../constants/Config';
 
 class SideNav extends Component {
   constructor(props) {
@@ -11,23 +11,23 @@ class SideNav extends Component {
   render() {
     return (
       <aside className={`SideNav`}>
-        <Link className={`SideNav-link`} to={`${LOCALHOST}/`}>
+        <Link className={`SideNav-link`} to={`${HOST}/`}>
           <span className={`ic ic_menu_home`} />
           <span>首頁</span>
         </Link>
-        <Link className={`SideNav-link`} to={`${LOCALHOST}/`}>
+        <Link className={`SideNav-link`} to={`${HOST}/`}>
           <span className={`ic ic_menu_favorite`} />
           <span>最愛歌曲</span>
         </Link>
-        <Link className={`SideNav-link`} to={`${LOCALHOST}/`}>
+        <Link className={`SideNav-link`} to={`${HOST}/`}>
           <span className={`ic ic_menu_requestbook`} />
           <span>點歌本</span>
         </Link>
-        <Link className={`SideNav-link`} to={`${LOCALHOST}/`}>
+        <Link className={`SideNav-link`} to={`${HOST}/`}>
           <span className={`ic ic_menu_requestinglist`} />
           <span>點歌清單</span>
         </Link>
-        <Link className={`SideNav-link`} to={`${LOCALHOST}/`}>
+        <Link className={`SideNav-link`} to={`${HOST}/`}>
           <span className={`ic ic_menu_history`} />
           <span>歷史紀錄</span>
         </Link>
--- app/constants/Config.js
+++ app/constants/Config.js
@@ -1,1 +1,2 @@
-export const API_ROOT = ''
+export const API_ROOT = '';
+export const HOST = '';
