67d32c110e70f6dd84d727d30f16aab6b2f81f05
Cesar Chen <cesar6134@gmail.com>
2015-10-05 15:27:32 +0800

    Added logo image into header

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,4 +1,6 @@
 import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
+import LOGO_IMG from '../assets/images/img_topbar_ktvStation.png';
 
 class Header extends Component {
   constructor(props) {
@@ -9,7 +11,9 @@ class Header extends Component {
     return (
       <header className={`Header`}>
         <div className={`Header-left`}>
-
+          <Link className={`Header-logo`} to={`/`}>
+            <img src={LOGO_IMG} alt="Logo" />
+          </Link>
         </div>
         <div className={`Header-right`}>
 
