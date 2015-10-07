d9b9fcec5d7f3075e334864e8dc5325ecc7ee4c6
Cesar Chen <cesar6134@gmail.com>
2015-10-08 07:52:24 +0800

    Updated the version of React to `0.14.0`

--- app/index.js
+++ app/index.js
@@ -2,10 +2,13 @@ import 'babel-core/polyfill';
 import './styles/styles.styl';
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, Redirect } from 'react-router';
 import createHistory from 'history/lib/createHashHistory';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
+// let IMGS = require.context('./assets/images', true, /\.(png|jpg|svg)$/igm);
+// IMGS.keys().forEach( key => console.log(IMGS(key)) );
 
 import { ROOT } from './constants/Config';
 
@@ -20,16 +23,14 @@ const history = createHistory({
 
 const store = configureStore();
 
-React.render(
+ReactDOM.render(
   <Provider store={store}>
-    {() =>
-      <Router history={history}>
-        <Route path={`${ROOT}/`} component={App}>
-          <IndexRoute component={HomePage}></IndexRoute>
-          <Route path={`playlist`} component={PlaylistPage} />
-        </Route>
-      </Router>
-    }
+    <Router history={history}>
+      <Route path={`${ROOT}/`} component={App}>
+        <IndexRoute component={HomePage}></IndexRoute>
+        <Route path={`playlist`} component={PlaylistPage} />
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
