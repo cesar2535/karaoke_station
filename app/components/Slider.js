cf1940c76d6ff9001f1c3b02c9fc9a1618308988
Cesar Chen <cesar6134@gmail.com>
2015-10-07 17:53:27 +0800

    Added `Slider` component and style

--- app/components/Slider.js
+++ app/components/Slider.js
@@ -1,12 +1,13 @@
 import React, { Component, PropTypes, findDOMNode } from 'react';
+import chunk from 'lodash/array/chunk';
 
 class Slider extends Component {
   static propTypes = {
-
+    children: PropTypes.array.isRequired
   }
 
-  static defaultProps = {
-
+  state = {
+    current: 0
   }
 
   constructor(props) {
@@ -15,11 +16,58 @@ class Slider extends Component {
 
   render() {
     const { children } = this.props;
+    const { current } = this.state;
+    const slides = chunk(children, 2);
+
     return (
       <div className={`Slider`}>
-
+        {this.renderPreviousBtn(current)}
+        {this.renderNextBtn(current, slides.length)}
+        <div className={`Slider-list`}>
+          {slides[current]}
+        </div>
+        {this.renderDots(slides)}
       </div>
     )
+  }
+
+  renderPreviousBtn(current) {
+    return current === 0 ? null : <div className={`Slider-previousBtn ic btn_page_previous`} onClick={this.handlePrevious.bind(this, current)}></div> ;
+  }
+
+  handlePrevious(current) {
+    let previous = current - 1;
+    if (previous < 0) {
+      previous = 0;
+    }
+    this.setState(Object.assign(this.state, { current: previous }));
+  }
+
+  renderNextBtn(current, length) {
+    return current === length - 1 ? null : <div className={`Slider-nextBtn ic btn_page_next`} onClick={this.handleNext.bind(this, current, length)}></div> ;
+  }
+
+  handleNext(current, length) {
+    let next = current + 1;
+    const final = length - 1;
+    if (next > final) {
+      next = final;
+    }
+    this.setState(Object.assign(this.state, { current: next }));
+  }
+
+  renderDots(slides) {
+    return (
+      <div className={`Slider-dots`}>
+        {slides.map(this.renderDot.bind(this))}
+      </div>
+    );
+  }
+
+  renderDot(item, index) {
+    const { current } = this.state;
+    const className = index === current ? `Slider-dot is-current` : `Slider-dot`;
+    return <div key={index} className={className} onClick={ evt => this.setState({ current: index })}></div>
   }
 }
 
--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,14 +1,130 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+import { ROOT } from '../constants/Config';
+import { loadPlaylist } from '../actions/playlist';
 
 import Slider from '../components/Slider';
+import List from '../components/List';
 
 class HomePage extends Component {
+
+  componentWillMount() {
+
+  }
+
   render() {
+    const {
+      queue, queueState,
+      favorites, favoritesState,
+      historyList, historyListState
+    } = this.props;
+
     return (
       <div className={`Page Page--home`}>
+        <Slider>
+          {this.renderFavorites(favorites, favoritesState)}
+          {this.renderSongBook()}
+          {this.renderQueue(queue, queueState)}
+          {this.renderHistory(historyList, historyListState)}
+        </Slider>
       </div>
     )
+  }
+
+  renderFavorites(favorites = [], favoritesState = {}) {
+    return (
+      <section className={`Favorites`}>
+        <h1>
+          <span className={`ic ic_menu_favorite`} />
+          <Link className={``} to={`${ROOT}/favorite`}>最愛歌曲</Link>
+        </h1>
+        <List className={`List--favorite`}
+              items={favorites.slice(0, 8)}
+              renderItem={this.renderListInFavorites.bind(this)}
+              isFetching={favoritesState.isFetching} />
+      </section>
+    );
+  }
+
+  renderListInFavorites(item, index) {
+    return (
+      <Link className={``} to={`${ROOT}/favorite/${item.name}`}>
+        <span>{item.name}</span>
+        <div className={`divider`}></div>
+        <span>{`${item.nSongs} ${item.nSongs === 1 ? 'song' : 'songs' }`}</span>
+      </Link>
+    );
+  }
+
+  renderSongBook() {
+    return (
+      <section className={`Songbook`}>
+        <h1>
+          <span className={`ic ic_menu_requestbook`} />
+          <span className={``}>點歌本</span>
+        </h1>
+        <div className={``}>
+          <Link className={``} to={`${ROOT}/songbook/category/male`}>
+            <h2>依歌星找歌</h2>
+          </Link>
+          <Link className={``} to={`${ROOT}/songbook/lang/Mandarin`}>
+            <h2>依語言找歌</h2>
+          </Link>
+        </div>
+      </section>
+    );
+  }
+
+  renderQueue(queue = [], queueState = {}) {
+    return (
+      <section className={`Queue`}>
+        <h1>
+          <span className={`ic ic_menu_requestinglist`} />
+          <Link className={``} to={`${ROOT}/playlist/current`}>點歌清單</Link>
+        </h1>
+        <List className={`List--queue`}
+              items={queue.slice(0, 8)}
+              renderItem={this.renderSongInQueue.bind(this)}
+              isFetching={queueState.isFetching} />
+        <Link to={`${ROOT}/playlist/current`}>More</Link>
+      </section>
+    );
+  }
+
+  renderSongInQueue(item, index) {
+    return (
+      <div key={index} className={`Song Song--queue`}>
+        <span className={`Song-title`}>{item.name}</span>
+        <span className={`Song-artist`}>{item.artist}</span>
+      </div>
+    );
+  }
+
+  renderHistory(historyList = [], historyListState = {}) {
+    return (
+      <section className={`History`}>
+        <h1>
+          <span className={`ic ic_menu_history`} />
+          <Link className={``} to={`${ROOT}/history`}>歷史紀錄</Link>
+        </h1>
+        <List className={`List--history`}
+              items={historyList.slice(0, 8)}
+              renderItem={this.renderSongInHistory.bind(this)}
+              isFetching={historyListState.isFetching} />
+      </section>
+    );
+  }
+
+  renderSongInHistory(item, index) {
+    return (
+      <div key={index} className={`Song Song--history`}>
+        <span className={`Song-title`}>{item.name}</span>
+        <span className={`Song-artist`}>{item.artist}</span>
+        <span className={`Song-time`}>{item.date}</span>
+      </div>
+    );
   }
 }
 
@@ -18,4 +134,4 @@ function mapStateToProps(state, ownProps) {
   };
 }
 
-export default connect(mapStateToProps)(HomePage);
+export default connect(mapStateToProps, { loadPlaylist })(HomePage);
