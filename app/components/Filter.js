febd1bd37136793ba7caab3ffb513b4ac03f0521
Cesar Chen <cesar6134@gmail.com>
2015-10-12 11:42:24 +0800

    Fixed the inheritance of Filter and Pager components

--- app/components/Filter.js
+++ app/components/Filter.js
@@ -3,7 +3,7 @@ import ClassNames from 'classnames';
 
 class Filter extends Component {
   constructor(props) {
-
+    super(props);
   }
 
   render() {
--- app/components/Pager.js
+++ app/components/Pager.js
@@ -3,7 +3,7 @@ import ClassNames from 'classnames';
 
 class Pager extends Component {
   constructor(props) {
-
+    super(props);
   }
 
   render() {
