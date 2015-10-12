dab94dbf36d07714d8ae3b5bf3974c4334772ddb
Cesar Chen <cesar6134@gmail.com>
2015-10-12 16:29:09 +0800

    Added the Playlist page and action. Fixed the schema for the playlist song data.

--- app/actions/playlist.js
+++ app/actions/playlist.js
@@ -1,20 +1,21 @@
 import { CALL_API, Schemas } from '../middleware/api';
 import { LOAD_PLAYLIST_REQUEST, LOAD_PLAYLIST_SUCCESS, LOAD_PLAYLIST_FAILURE } from '../constants/ActionTypes';
 
-function fetchPlaylist(name) {
+function fetchPlaylist(name, page = 1, count = 20) {
   return {
     name,
+    page,
     [CALL_API]: {
       types: [LOAD_PLAYLIST_REQUEST, LOAD_PLAYLIST_SUCCESS, LOAD_PLAYLIST_FAILURE],
-      endpoint: `/playlist?state=${name}`,
-      schema: Schemas.SONG_ARRAY,
+      endpoint: `/playlist?state=${name}&page=${page}&count=${count}`,
+      schema: Schemas.PLAYLIST,
       method: 'GET'
     }
   };
 }
 
-export function loadPlaylist(name) {
+export function loadPlaylist(name, page, count) {
   return dispatch => {
-    return dispatch(fetchPlaylist(name));
+    return dispatch(fetchPlaylist(name, page, count));
   };
 };
--- app/containers/PlaylistPage.js
+++ app/containers/PlaylistPage.js
@@ -1,24 +1,132 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+import { ROOT } from '../constants/Config';
+
+import List from '../components/List';
+import SideNav from '../components/SideNav';
+import Filter from '../components/Filter';
+import Pager from '../components/Pager';
+
+import { loadPlaylist } from '../actions/playlist';
+
+function loadData(props) {
+  props.loadPlaylist('current');
+  props.loadPlaylist('finished');
+}
 
 class PlaylistPage extends Component {
   constructor(props) {
     super(props);
   }
 
+  componentWillMount() {
+    loadData(this.props);
+  }
+
   render() {
+    const { query } = this.props.location;
+    const { queue, finished } = this.props;
+
     return (
       <div className={`Page Page--playlist`}>
-        
+        <SideNav />
+        {this.renderTabs(queue, finished)}
+        {this.renderContent(query, this.props)}
+      </div>
+    );
+  }
+
+  renderTabs(queue, finished) {
+    return (
+      <div className={`SideTab`}>
+        <Link className={`SideTab-link`} to={`${ROOT}/playlist`} query={{ list: `current` }} activeClassName={`is-current`}>
+          {`待播清單 (${queue.length})`}
+        </Link>
+        <Link className={`SideTab-link`} to={`${ROOT}/playlist`} query={{ list: `finished` }} activeClassName={`is-current`}>
+          {`已播清單 (${finished.length})`}
+        </Link>
+      </div>
+    );
+  }
+
+  renderContent(query, props) {
+    const { queue, finished, songsByQueue, songsByFinished } = props;
+
+    switch (query.list) {
+      case 'current':
+        return (
+          <div className={`Page-content`}>
+            <Filter />
+            <section className={`Page-main`}>
+              <h1>{`待播清單`}</h1>
+              <div className={`Queue Queue--playlist`}>
+                <div className={`Queue-head`}>
+                  <div>歌名</div>
+                  <div>演唱者</div>
+                </div>
+                <List className={`List--queue Queue-body`}
+                      items={queue}
+                      renderItem={this.renderListItem.bind(this)}
+                      isFetching={songsByQueue.isFetching} />
+              </div>
+            </section>
+            <Pager />
+          </div>
+        );
+      case 'finished':
+        return (
+          <div className={`Page-content`}>
+            <Filter />
+            <section className={`Page-main`}>
+              <h1>{`已播清單`}</h1>
+              <div className={`Queue Queue--playlist`}>
+                <div className={`Queue-head`}>
+                  <div>歌名</div>
+                  <div>演唱者</div>
+                </div>
+                <List className={`List--queue Queue-body`}
+                      items={finished}
+                      renderItem={this.renderListItem.bind(this)}
+                      isFetching={songsByFinished.isFetching} />
+              </div>
+            </section>
+            <Pager />
+          </div>
+        );
+      default:
+        throw new Error('Location query is incorrect.');
+    }
+  }
+
+  renderListItem(item, index) {
+    return (
+      <div key={index} className={`Song`}>
+        <span className={``}>{item.name}</span>
+        <span className={``}>{item.artist}</span>
       </div>
     );
   }
 }
 
 function mapStateToProps(state, ownProps) {
-  return {
+  const {
+    pagination: { songsFromPlaylist },
+    entities: { songsInPlaylist }
+  } = state;
 
-  }
+  const songsByQueue = songsFromPlaylist['current'] || { ids: [], page: 0 };
+  const songsByFinished = songsFromPlaylist['finished'] || { ids: [], page: 0 };
+  const queue = songsByQueue.ids.map(id => songsInPlaylist[id]);
+  const finished = songsByFinished.ids.map(id => songsInPlaylist[id]);
+
+  return {
+    songsByQueue,
+    songsByFinished,
+    queue,
+    finished
+  };
 }
 
-export default connect(mapStateToProps)(PlaylistPage);
+export default connect(mapStateToProps, { loadPlaylist })(PlaylistPage);
