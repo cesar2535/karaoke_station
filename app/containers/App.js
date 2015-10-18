b0be60b75ee92b2225f1b1f447cb396f4bdd3592
Cesar Chen <cesar6134@gmail.com>
2015-10-18 22:39:47 +0800

    Added the SlidePanel trigger button

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import { findDOMNode } from 'react-dom'
 import { connect } from 'react-redux';
 
 import Header from '../components/Header';
@@ -10,12 +11,19 @@ class App extends Component {
     const { children } = this.props;
     return (
       <div className={`App`}>
-        <Header />
+        <Header toggleSearch={this.toggleSearch.bind(this)} />
         {children}
-        <SlidePanel />
+        <SlidePanel ref="slidePanel" className={`SlidePanel`} />
         <Footer />
       </div>
     );
+  }
+
+  toggleSearch(evt) {
+    const { slidePanel } = this.refs;
+    findDOMNode(slidePanel).classList.add('is-visible');
+    evt.stopPropagation();
+    evt.nativeEvent.stopImmediatePropagation();
   }
 }
 
--- app/containers/SlidePanel.js
+++ app/containers/SlidePanel.js
@@ -1,17 +1,42 @@
 import React, { Component, PropTypes } from 'react';
+import { findDOMNode } from 'react-dom';
 import { connect } from 'react-redux';
 import ClassNames from 'classnames';
+
+import SearchForm from '../components/SearchForm';
 
 class SlidePanel extends Component {
   constructor(props) {
     super(props);
   }
 
+  componentDidMount() {
+    document.addEventListener('click', this.togglePanel.bind(this));
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.togglePanel.bind(this));
+  }
+
   render() {
+    const { className } = this.props;
     return (
-      <div className={`SlidePanel`}>
+      <div ref='panel' className={className}>
+        <div className={`SlideContainer`} onClick={this.stopPropagation.bind(this)}>
+
+        </div>
       </div>
     );
+  }
+
+  togglePanel(evt) {
+    const { panel } = this.refs;
+    findDOMNode(panel).classList.remove('is-visible');
+  }
+
+  stopPropagation(evt) {
+    evt.stopPropagation();
+    evt.nativeEvent.stopImmediatePropagation();
   }
 }
 
